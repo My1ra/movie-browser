0c3ac5d7a74f99cffee7a5489fc92e374f29fc4f
Myra <myra@singtecs.com>
2025-06-16 21:20:12 +0700

    Remove environment files from repo to protect sensitive info

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  tmdbApiKey: '2dbeeca12a2760882fe4936ebf7e1dc6',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  tmdbApiKey: '2dbeeca12a2760882fe4936ebf7e1dc6'
-};
-
