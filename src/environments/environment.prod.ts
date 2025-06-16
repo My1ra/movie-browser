9faec1d0f6e4a300e1ac3475c179bc41321e964c
Myra <myra@singtecs.com>
2025-06-16 22:34:05 +0700

    Add placeholder environment.prod.ts for Vercel

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+// export const environment = {
+//   production: true,
+//   tmdbApiKey: '2dbeeca12a2760882fe4936ebf7e1dc6',
+// };
+
+export const environment = {
+  production: true,
+  tmdbApiKey: 'TMDB_API_KEY_FROM_ENV'
+};
+
